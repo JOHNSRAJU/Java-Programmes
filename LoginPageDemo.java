a48c42eaf577ccfefb5d54315629a55253611ee8
Johns Raju <83854933+JOHNSRAJU@users.noreply.github.com>
2022-02-03 21:09:52 +0530

    Add files via upload

--- LoginPageDemo.java
+++ LoginPageDemo.java
@@ -1,33 +1,37 @@
 import java.awt.*;
 import java.awt.event.*;
-public class LoginPageDemo extends Frame implements ActionListener  {
+public class LoginPageDemo extends Frame implements ActionListener {
 	Button button;
 	TextField textField1 = new TextField(20);
 	TextField textField2 = new TextField(20);
-	 
+	TextField textField3 = new TextField(20);
+	TextField textField4 = new TextField(20);
 	LoginPageDemo(){
 		
-		setTitle("Login Page");
+		setTitle("Sign up page");
 		setSize(400,400);
 		setVisible(true);
 		setLayout(new FlowLayout());
-		
-		
 		button=new Button("SUBMIT");
 		add(textField1);
 		add(textField2);
+		add(textField3);
+		add(textField4);
 		add(button);
-		textField1.setText("UserName");
-		textField2.setText("Password");
+		textField1.setText("Name");
+		textField2.setText("E-mail");
+		textField3.setText("Mob Number");
+		textField4.setText("Place");
 		button.addActionListener(this);
-		textField1.addActionListener(this);
-		textField2.addActionListener(this);
 		
 		
 	}
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("User name :- "+textField1.getText());
-		System.out.println("PASSWORD  :- "+textField2.getText());
+		
+		System.out.println("Name :- "+textField1.getText());
+		System.out.println("E-mail  :- "+textField2.getText());
+		System.out.println("Mob Number  :- "+textField3.getText());
+		System.out.println("Place  :- "+textField4.getText());
 		
 	}
 	
