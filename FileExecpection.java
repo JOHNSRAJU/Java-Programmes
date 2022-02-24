f62982a917e5a4e0e6c4a90286faab42de9e36b6
Johns Raju <83854933+JOHNSRAJU@users.noreply.github.com>
2022-02-24 16:24:37 +0530

    Add files via upload

--- FileExecpection.java
+++ FileExecpection.java
@@ -6,17 +6,23 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileExecpection {
-	public static void main(String[] args) throws IOException,FileNotFoundException {
-		
-		FileWriter fw = new FileWriter("CopySample.txt");
-		FileReader fr = new FileReader("4Sample.txt");
-		int i=0;
-		while((i=fr.read())!=-1) {
-			char j=(char)i;
-			fw.write(j);
+	public static void main(String[] args) {
+		try {
+			FileWriter fw = new FileWriter("CopySample.txt");
+			FileReader fr = new FileReader("4Sample.txt");
+			int i=0;
+			while((i=fr.read())!=-1) {
+				char j=(char)i;
+					fw.write(j);
+			}
+			fr.close();
+			fw.close();
+		}catch(FileNotFoundException e ) {
+			System.out.println("Exception;File NOT Found Sorry");
 		}
-		fr.close();
-		fw.close();
+		catch(IOException e){
+			System.out.println("Exception;IOException Found Sorry");
+		}
 	}
 
 }
